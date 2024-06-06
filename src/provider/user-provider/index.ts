70d32d2725c4fc20b434949cab6dbf7137f2c7a0
GEdO23 <rm99632@fiap.com.br>
2024-06-06 18:47:01 -0300

    Movendo forgot-password e alterando user provider

--- src/provider/user-provider/index.ts
+++ src/provider/user-provider/index.ts
@@ -1,6 +1,5 @@
 import UserContext from "./user-context";
 import UserProvider from "./UserProvider";
-import UserProps from "./user-props";
 
 export default UserProvider;
-export { UserContext, UserProps };
+export { UserContext };
